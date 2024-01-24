73171e03483bfdfc415bd11ebb04a66518f0228b
Macho99 <pes4825@naver.com>
2024-01-24 21:43:04 +0900

    feat : device swap chain 적용

--- DirectX11Practice/Game.cpp
+++ DirectX11Practice/Game.cpp
@@ -14,6 +14,9 @@ void Game::Init(HWND hwnd)
 	_hwnd = hwnd;
 	_width = GWinSizeX;
 	_height = GWinSizeY;
+
+	// TODO
+	CreateDeviceAndSwapChain();
 }
 
 void Game::Update()
@@ -22,4 +25,57 @@ void Game::Update()
 
 void Game::Render()
 {
+}
+
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = _width;
+		desc.BufferDesc.Height = _height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1;
+		desc.OutputWindow = _hwnd;
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		_swapChain.GetAddressOf(),
+		_device.GetAddressOf(),
+		nullptr,
+		_deviceContext.GetAddressOf()
+	);
+
+	CHECK(hr);
+}
+
+void Game::CreateRenderTargetView()
+{
+	HRESULT hr;
+
+	ComPtr<ID3D11Texture2D> backBuffer = nullptr;
+	hr = _swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D),
+		(void**)backBuffer.GetAddressOf());
+	CHECK(hr);
+
+	_device->CreateRenderTargetView(backBuffer.Get(), nullptr, 
+		_renderTargetView.GetAddressOf());
+	CHECK(hr);
 }
--- DirectX11Practice/Game.h
+++ DirectX11Practice/Game.h
@@ -1,11 +1,6 @@
 #pragma once
 class Game
 {
-private:
-	HWND _hwnd;
-	uint32 _width = 0;
-	uint32 _height = 0;
-
 public:
 	Game();
 	virtual ~Game();
@@ -15,6 +10,30 @@ public:
 	void Update();
 	void Render();
 
+private:
+	HWND _hwnd = nullptr;
+	uint32 _width = 0;
+	uint32 _height = 0;
+
+private:
+	// Device & Swap
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+	
+	// RTV
+	ComPtr<ID3D11RenderTargetView> _renderTargetView = nullptr;
+
+	// Misc
+	D3D11_VIEWPORT _viewport = { 0 };
+
+private:
+	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+
+private:
+	void RenderBegin();
+	void RenderEnd();
 
 };
 
